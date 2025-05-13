add519ea87e04c7293494d1c1bdac1549149f040
anujsc <anujpvt2311@gmail.com>
2025-05-13 13:51:46 +0530

    4th com

--- src/utilis/Api.jsx
+++ src/utilis/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = "wxrmo3alnlldsdqjq";
 console.log('API Key:', API_KEY);
 
 const BASE_URL = "https://techhk.aoscdn.com/";
