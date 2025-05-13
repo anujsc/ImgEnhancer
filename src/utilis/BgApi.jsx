0988dfb12e0c3648f4b448fc3146798563e8e974
anujsc <anujpvt2311@gmail.com>
2025-05-13 19:58:12 +0530

    4th com

--- src/utilis/BgApi.jsx
+++ src/utilis/BgApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY =" wxrmo3alnlldsdqjq";
 const BASE_URL = "https://techhk.aoscdn.com/";
 const MAX_RETRIES = 20;
 
